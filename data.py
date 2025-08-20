8ddb24ad9190d5f1141dd7304db8d2dd50fdc432
Dracma1990 <vargasandres.md@gmail.com>
2025-08-20 18:04:15 -0500

    Cambios 20-08-2025

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-d471e114-cf01-4599-9c00-21c1ff9d9c6b.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-a46e95e4-a987-470e-bd05-8aeeed6f3d3e.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
